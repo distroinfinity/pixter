71cb4ce7dc714d5bae5bc2bb74efea7c3cca1bb1
Kevin Joshi <kevinjoshi46b@gmail.com>
2023-05-05 18:19:37 +0530

    ft: completed basic UI of home page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,46 +1,80 @@
 import Head from "next/head";
 import Link from "next/link";
+import Avatar from "avataaars";
 import type { NextPage } from "next";
-import { BugAntIcon } from "@heroicons/react/24/outline";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Pixter</title>
+        <title>Pixter: Home</title>
         <meta name="description" content="Create your very own Web3 Avatar" />
       </Head>
 
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/nextjs/pages/index.tsx</code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract <code className="italic bg-base-300 text-base font-bold">YourContract.sol</code> in{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/hardhat/contracts</code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+      <Link href="/edit/0" className="mt-8 mx-auto">
+        <button className="btn btn-secondary">Mint New Avatar ✨</button>
+      </Link>
+      <div className="flex flex-row flex-wrap mx-auto mb-10">
+        <Link href="/edit/1">
+          <div className="card card-compact w-11/12 lg:w-80 bg-base-100 shadow-xl p-3 mx-auto my-0 mt-10 lg:mx-4 items-center">
+            <Avatar
+              style={{ width: "100%", height: "100%" }}
+              avatarStyle="Transparent"
+              topType="ShortHairShortWaved"
+              accessoriesType="Blank"
+              hairColor="Black"
+              facialHairType="Blank"
+              clotheType="Hoodie"
+              clotheColor="Black"
+              eyeType="Happy"
+              eyebrowType="Default"
+              mouthType="Smile"
+              skinColor="Light"
+            />
+            <h2 className="text-2xl font-bold mt-4">Kevin</h2>
           </div>
-        </div>
+        </Link>
+
+        <Link href="/edit/2">
+          <div className="card card-compact w-11/12 lg:w-80 bg-base-100 shadow-xl p-3 mx-auto my-0 mt-10 lg:mx-4 items-center">
+            <Avatar
+              style={{ width: "100%", height: "100%" }}
+              avatarStyle="Transparent"
+              topType="Hat"
+              accessoriesType="Sunglasses"
+              hairColor="Black"
+              facialHairType="Blank"
+              clotheType="GraphicShirt"
+              clotheColor="Black"
+              graphicType="Pizza"
+              eyeType="Happy"
+              eyebrowType="Default"
+              mouthType="Eating"
+              skinColor="Light"
+            />
+            <h2 className="text-2xl font-bold mt-4">Kevin Cool</h2>
+          </div>
+        </Link>
+
+        <Link href="/edit/3">
+          <div className="card card-compact w-11/12 lg:w-80 bg-base-100 shadow-xl p-3 mx-auto my-0 mt-10 lg:mx-4 items-center">
+            <Avatar
+              style={{ width: "100%", height: "100%" }}
+              avatarStyle="Transparent"
+              topType="ShortHairShortWaved"
+              accessoriesType="Prescription02"
+              hairColor="Black"
+              facialHairType="Blank"
+              clotheType="CollarSweater"
+              clotheColor="Black"
+              eyeType="Happy"
+              eyebrowType="Default"
+              mouthType="Serious"
+              skinColor="Light"
+            />
+            <h2 className="text-2xl font-bold mt-4">Kevin Nerd</h2>
+          </div>
+        </Link>
       </div>
     </>
   );
